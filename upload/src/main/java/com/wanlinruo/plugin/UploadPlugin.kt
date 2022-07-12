d6da68b20037a5d6707d8b5df4d2162e1f868cb9
wanlinruo <ln.wan@tuya.com>
2022-07-13 00:10:14 +0800

    feat:增加本地upload的功能

--- upload/src/main/java/com/wanlinruo/plugin/UploadPlugin.kt
+++ upload/src/main/java/com/wanlinruo/plugin/UploadPlugin.kt
@@ -1,4 +1,4 @@
-package com.wanlinruo.upload
+package com.wanlinruo.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -6,6 +6,6 @@ import org.gradle.api.Project
 class UploadPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-
+        println("====================== UploadPlugin")
     }
 }
